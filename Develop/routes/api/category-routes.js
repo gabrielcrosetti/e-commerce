82b9fd47301785d373810bc5817646c712fc4e98
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-10-28 23:31:25 -0400

    Update category routes, debug file structure

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -50,9 +50,7 @@ router.get("/:id", (req, res) => {
 
 router.post("/", (req, res) => {
   // create a new category
-  Category.create({
-    category_name: req.body.category_name
-  })
+  Category.create(req.body)
   .then(dbCategoryData => res.json(dbCategoryData))
     .catch(err => {
       console.log(err);
@@ -62,10 +60,29 @@ router.post("/", (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
-});
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+})
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
