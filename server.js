36ac77ad25f4d86a70ccbb0305fcfeecf5c6a542
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-10-28 20:45:59 -0400

    update category.js model

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const routes = require('./Develop/routes');
+// import sequelize connection
+const mysql = require('mysql2');
+const sequelize = require('./Develop/config/connection');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('App listening on port ${PORT}!'));
+});
